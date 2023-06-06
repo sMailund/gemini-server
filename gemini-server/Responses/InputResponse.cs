7c3d2143fbad2723665ef9902ca3e19e857927c5
Simen Mailund Svendsen <sima@netlight.com>
2023-06-06 18:51:59 +0200

    add input response

---
+++ gemini-server/Responses/InputResponse.cs
@@ -0,0 +1,15 @@
+namespace gemini_server.Responses;
+
+public class InputResponse : IResponse
+{
+   public InputResponse(string prompt)
+   {
+      Prompt = prompt;
+   }
+   
+   public string Prompt { get; init; }
+   
+   public int GetStatusCode() => 10;
+
+   public string GetMeta() => Prompt;
+}
