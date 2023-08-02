eb2868d4658f83595649f4d59d2f3146df1bcb8d
Simen Mailund Svendsen <simen.m.s@hotmail.com>
2023-08-02 15:23:11 +0200

    frontpage

--- HackerNews/Handlers/FrontPageHandler.cs
+++ HackerNews/Handlers/FrontPageHandler.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using gemini_server;
 using gemini_server.Responses;
 
@@ -7,6 +8,31 @@ public class FrontPageHandler
 {
     public IResponse Handle(Request req)
     {
-        return new SuccessResponse($"handler works!!! And your name is {req.UserName}");
+        var posts = new List<Post>
+        {
+            new("This is a test post"),
+            new("This is another post"),
+        };
+
+        var greeting = req.IsLoggedIn
+            ? $"Welcome back, {req.UserName}!"
+            : "Welcome to Gemtalk! Please log in to join the discussion.";
+
+        var formattedPosts = posts
+            .Select(it => $"## {it.Title}");
+
+        var sb = new StringBuilder()
+            .AppendLine("# gemtalk")
+            .AppendLine(greeting)
+            .AppendLine()
+            .AppendLine();
+
+        foreach (var post in formattedPosts)
+        {
+            sb.AppendLine(post);
+            sb.AppendLine();
+        }
+
+        return new SuccessResponse(sb.ToString());
     }
 }
