031cc792307b9deb82d3793b72c6c1637e851d2f
Simen Mailund Svendsen <simen.m.s@hotmail.com>
2023-08-03 10:52:27 +0200

    change post to class

--- HackerNews/Post.cs
+++ HackerNews/Post.cs
@@ -2,4 +2,35 @@ using System.Security.Cryptography.X509Certificates;
 
 namespace HackerNews;
 
-internal record Post(string Title, string Link, string PostedByUser, Guid PostId, string PostedByUserId, DateTime PostedAt, int Points);
+internal class Post
+{
+    public Post(string Title, string Link, string PostedByUser, Guid PostId, string PostedByUserId, DateTime PostedAt, int Points)
+    {
+        this.Title = Title;
+        this.Link = Link;
+        this.PostedByUser = PostedByUser;
+        this.PostId = PostId;
+        this.PostedByUserId = PostedByUserId;
+        this.PostedAt = PostedAt;
+        this.Points = Points;
+    }
+
+    public string Title { get; init; }
+    public string Link { get; init; }
+    public string PostedByUser { get; init; }
+    public Guid PostId { get; init; }
+    public string PostedByUserId { get; init; }
+    public DateTime PostedAt { get; init; }
+    public int Points { get; init; }
+
+    public void Deconstruct(out string Title, out string Link, out string PostedByUser, out Guid PostId, out string PostedByUserId, out DateTime PostedAt, out int Points)
+    {
+        Title = this.Title;
+        Link = this.Link;
+        PostedByUser = this.PostedByUser;
+        PostId = this.PostId;
+        PostedByUserId = this.PostedByUserId;
+        PostedAt = this.PostedAt;
+        Points = this.Points;
+    }
+}
