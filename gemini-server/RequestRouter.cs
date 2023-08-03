1a2470d4eb49649ec120f2fbd0a8ef005ce25050
Simen Mailund Svendsen <simen.m.s@hotmail.com>
2023-08-03 16:53:26 +0200

    add wildcard matchin

--- gemini-server/RequestRouter.cs
+++ gemini-server/RequestRouter.cs
@@ -5,7 +5,7 @@ namespace gemini_server;
 public class RequestRouter
 {
     private Dictionary<string, Func<Request, IResponse>> _handlers = new();
-    
+
     public void RegisterHandler(string path, Func<Request, IResponse> handler)
     {
         _handlers[path] = handler;
@@ -13,13 +13,43 @@ public class RequestRouter
 
     public IResponse HandleRequest(Request request)
     {
-        var handler = _handlers.GetValueOrDefault(request.Uri.LocalPath);
-
-        if (handler == null)
+        foreach (var kvp in _handlers)
         {
-            return new NotFoundResponse();
+            var route = kvp.Key;
+            var handler = kvp.Value;
+
+            if (IsMatchingRoute(request.Uri.LocalPath, route))
+            {
+                return handler(request);
+            }
         }
-        
-        return handler(request);
+
+        return new NotFoundResponse();
+    }
+
+    private bool IsMatchingRoute(string requestPath, string route)
+    {
+        var requestSegments = requestPath.Trim('/').Split('/');
+        var routeSegments = route.Trim('/').Split('/');
+
+        if (requestSegments.Length != routeSegments.Length)
+        {
+            return false;
+        }
+
+        for (int i = 0; i < routeSegments.Length; i++)
+        {
+            if (routeSegments[i] == "*")
+            {
+                continue; // Wildcard, matches any segment
+            }
+
+            if (routeSegments[i] != requestSegments[i])
+            {
+                return false; // Not a match
+            }
+        }
+
+        return true;
     }
 }
